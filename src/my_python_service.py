43df7afe8cb3f29c57d64d85a08832009f88271f
MajorX234 <majorx234@googlemail.com>
2024-09-04 23:59:57 +0200

    Add first systemd service example script

---
+++ src/my_python_service.py
@@ -0,0 +1,24 @@
+import time
+import systemd.daemon
+
+def main():
+    print('Starting my python service ...')
+    time.sleep(10)
+    print('Startup complete')
+
+    # Tell systemd that our service is ready
+    systemd.daemon.notify('READY=1')
+
+    running = True
+    steps = 0
+    while running:
+        print('my python service is running (step: {})'.format(steps))
+        if steps == 10:
+            break
+        steps += 1
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
+    print('closing my python service')
